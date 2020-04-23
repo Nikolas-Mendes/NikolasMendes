93763cc04f644b1c2d68d2392dcfd9ef545b581d
Nikolas Mendes <nikolaspoi055@gmail.com>
2020-04-23 15:49:33 -0300

    Decimo Exercicio

--- Execicio10/src/main/resources/squema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists funcionario;
-
-create table funcionario(
-    id_funcionario bigint auto_incriment,
-    nome varchar(100) not null,
-    salario double not null,
-    rg int not null,
-    primary key(id_funcionario));
